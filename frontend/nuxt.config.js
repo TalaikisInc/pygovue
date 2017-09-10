4ee64b7591d0e42032761c33da7f1873e901c875
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2017-09-10 17:22:00 +0300

    fixes

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -76,7 +76,7 @@ module.exports = {
   },
   loading: '~/components/Loading.vue',
   build: {
-    analyze: true,
+    analyze: false,
     extractCSS: true,
     vendor: ['axios', 'moment'],
     extend (config, ctx) {
