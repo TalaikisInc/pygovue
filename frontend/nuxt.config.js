3440e7e9abbe68b69c013f1a88e2335b5c8777f9
23BFF0N\DXenu <talaikis.tadas@gmail.com>
2017-09-11 23:40:18 +0300

    updates

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -27,7 +27,6 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
       { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' }
     ]
   },
